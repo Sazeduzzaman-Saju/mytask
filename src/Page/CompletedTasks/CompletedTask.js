f53d76257efdbc0a70f1e32e23b002d35ba625cd
Sazeduzzaman <sazeduzzaman@yahoo.com>
2022-12-28 18:06:03 +0600

    All Task Page Done

--- src/Page/CompletedTasks/CompletedTask.js
+++ src/Page/CompletedTasks/CompletedTask.js
@@ -18,6 +18,44 @@ const CompletedTask = () => {
             link2: '/completed-task',
         },
     ]
+    const tasks = [
+        {
+            id: '1',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '2',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '3',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '4',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '5',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '6',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+    ]
 
     return (
         <section className='mx-w-full-xl'>
@@ -33,27 +71,32 @@ const CompletedTask = () => {
             <div className='text-start p-5'>
                 <h1 className='text-3xl text-black font-bold uppercase'>[ All Completed Task ]</h1>
             </div>
-            <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
-                <div className="flex justify-center md:justify-end -mt-16">
-                    <img className="w-20 h-20 object-cover rounded-full border-2  drop-shadow-lg" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt='' />
-                </div>
-                <div>
-                    <h2 className="text-gray-800 text-3xl font-semibold">Design Tools</h2>
-                    <p className="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
-                </div>
-                <div className="flex justify-center mt-4">
-
-                    {important ?
+            <div className='task_container'>
+                {
+                    tasks.map((item) => <div className="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+                        <div className="flex justify-center md:justify-end -mt-16">
+                            <img className="w-20 h-20 object-cover rounded-full border-2  drop-shadow-lg" src={item.profile_link} alt='' />
+                        </div>
                         <div>
-                            <button className='primary_btn bg-red' style={{ backgroundColor: "black", color: 'white' }}>Remove</button>
+                            <h2 className="text-gray-800 text-3xl font-semibold">{item.name}</h2>
+                            <p className="mt-2 text-gray-600">{item.task}</p>
+                        </div>
+                        <div className="flex justify-center mt-4">
 
+                            {important ?
+                                <div>
+                                    <button className='primary_btn bg-red' style={{ backgroundColor: "black", color: 'white' }}>Remove</button>
+
+                                </div>
+                                : <div>
+                                    <button className='primary_btn ml-2' onClick={handleClick}>Important</button>
+                                    <button className='primary_btn ml-2' >Remove</button>
+                                </div>
+                            }
                         </div>
-                        : <div>
-                            <button className='primary_btn ml-2' onClick={handleClick}>Important</button>
-                            <button className='primary_btn ml-2' >Remove</button>
-                        </div>
-                    }
-                </div>
+                    </div>
+                    )
+                }
             </div>
         </section>
     );
--- src/Page/My Task/MyTask.js
+++ src/Page/My Task/MyTask.js
@@ -18,6 +18,44 @@ const MyTask = () => {
             link2: '/add-task',
         },
     ]
+    const tasks = [
+        {
+            id: '1',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '2',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '3',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '4',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '5',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+        {
+            id: '6',
+            name: 'Design Tools',
+            task: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!',
+            profile_link: 'https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+        },
+    ]
 
     return (
         <section className='mx-w-full-xl'>
@@ -33,28 +71,33 @@ const MyTask = () => {
             <div className='text-center p-5'>
                 <h1 className='text-3xl text-black font-bold uppercase'>[ My All Task ]</h1>
             </div>
-            <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
-                <div class="flex justify-center md:justify-end -mt-16">
-                    <img class="w-20 h-20 object-cover rounded-full border-2 border-indigo-500" src="https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt='' />
-                </div>
-                <div>
-                    <h2 class="text-gray-800 text-3xl font-semibold">Design Tools</h2>
-                    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolores deserunt ea doloremque natus error, rerum quas odio quaerat nam ex commodi hic, suscipit in a veritatis pariatur minus consequuntur!</p>
-                </div>
-                <div class="flex justify-center mt-4">
-
-                    {important ?
+            <div className='task_container'>
+                {
+                    tasks.map((item) => <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
+                        <div class="flex justify-center md:justify-end -mt-16 drop-shadow-lg">
+                            <img class="w-20 h-20 object-cover rounded-full drop-shadow-lg" src={item.profile_link} alt='' />
+                        </div>
                         <div>
-                            <button className='primary_btn bg-red' style={{ backgroundColor: "black", color: 'white' }}>Remove</button>
+                            <h2 class="text-gray-800 text-3xl font-semibold">{item.name}</h2>
+                            <p class="mt-2 text-gray-600">{item.task}</p>
 
                         </div>
-                        : <div>
-                            <button className='primary_btn ml-2' onClick={handleClick}>Important</button>
-                            <button className='primary_btn ml-2' >Remove</button>
-                            <button className='primary_btn ml-2' >Replace</button>
+                        <div class="flex justify-center mt-4">
+
+                            {important ?
+                                <div>
+                                    <button className='primary_btn bg-red' style={{ backgroundColor: "black", color: 'white' }}>Remove</button>
+
+                                </div>
+                                : <div>
+                                    <button className='primary_btn ml-2' onClick={handleClick}>Important</button>
+                                    <button className='primary_btn ml-2' >Remove</button>
+                                    <button className='primary_btn ml-2' >Replace</button>
+                                </div>
+                            }
                         </div>
-                    }
-                </div>
+                    </div>)
+                }
             </div>
         </section>
     );
